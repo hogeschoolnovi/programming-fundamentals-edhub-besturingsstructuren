a8bc4dabe369c7150c9c6cfb2682e237931cc659
RowanPl <Rowanplooij@hotmail.com>
2024-04-26 09:12:02 +0200

    opgaves

---
+++ opgaves.py
@@ -0,0 +1,37 @@
+# Opgave 1:
+# Schrijf een programma waarbij je twee gehele positieve getallen kunt invoeren. Het programma bepaalt en toont of
+# het ene getal een veelvoud is van het andere getal.
+#
+#
+# Opgave 2:
+# De basisprijs van een bioscoopkaartje is 12 euro. Kinderen tot 5 jaar zijn gratis en kinderen van 5 tot 12 jaar betalen de halve prijs. Personen tussen 13 en 54 jaar moeten de volle prijs betalen en vanaf 55 jaar is de toegang weer gratis.
+# Maak een programma dat de te betalen prijs afdrukt nadat je de leeftijd hebt ingevoerd.
+#
+#
+# Opgave 3:
+# Schrijf een programma dat 3 gehele getallen sorteert. De getallen worden ingevoerd en respectievelijk opgeslagen in
+# de variabelen num1, num2 en num3. Het programma sorteert de getallen zodanig dat na afloop num1 ≤ num2 ≤ num3.
+#
+#
+# Opgave 4: Schrijf een programma dat gehele getallen leest, het totaal en het gemiddelde van deze getallen berekent
+# en afdrukt, waarbij 0 niet wordt meegeteld. Het programma eindigt met invoer 0.
+#
+#
+# Opgave 5: Schrijf een programma dat de tafel van “factor” afdrukt, nadat je een geheel getal tussen 0 en 10 (de
+# “factor”) hebt ingevoerd.
+#
+# Bijvoorbeeld als factor = 5:
+# Out:
+#
+# 1 x 5 = 5
+# 2 x 5 = 10
+# 3 x 5 = 15    # enz.
+#
+# Opgave 6:
+# Schrijf een programma met de volgende functionaliteit:
+#
+#     Eerst moet je een willekeurig getal invoeren (het “zoekgetal”).
+#     Daarna moet je een geheel getal tussen 0 en 10 invoeren (het “aantal”).
+#     Tenslotte voer je “aantal” getallen in (dus als aantal = 5 moet je 5 getallen invoeren).
+#
+# Het programma drukt alle getallen af die kleiner zijn dan het zoekgetal.
