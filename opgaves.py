5c464c25ce7d6657e2a0f3aecabe818ffefdb151
Danielooo <dvanraffe@gmail.com>
2024-08-28 17:38:20 +0200

    synced main with uitwerkingen

--- opgaves.py
+++ opgaves.py
@@ -1,37 +1,84 @@
+# ==========================================
+# Voorbeeld Opdracht
+# Gegeven zijn de variabelen a = 3 en b = 10. Evalueer met een if statement of a groter is dan b. Als dat zo is, print dan a. Als dat niet zo is, print dan b.
+# ==========================================
+
+a = 3
+b = 10
+
+if a > b:
+    print(a)
+else:
+    print(b)
+
+# ==========================================
 # Opgave 1:
-# Schrijf een programma waarbij je twee gehele positieve getallen kunt invoeren. Het programma bepaalt en toont of
-# het ene getal een veelvoud is van het andere getal.
-#
-#
+# Gegeven is een int input geatal1 en getal2 en drie print methodes. Schrijf een if statement dat controleert of getal1 een veelvoud is van getal2, andersom of dat beide getallen geen veelvoud zijn van de ander.
+# Zet de juiste print methode op de goede plek in je if statement.
+# Voorbeeld goede output: 10 is een veelvoud van 5
+# ==========================================
+
+
+getal1 = int(input("voer een getal in: "))
+getal2 = int(input("voer een getal in: "))
+
+
+
+# ==========================================
 # Opgave 2:
-# De basisprijs van een bioscoopkaartje is 12 euro. Kinderen tot 5 jaar zijn gratis en kinderen van 5 tot 12 jaar betalen de halve prijs. Personen tussen 13 en 54 jaar moeten de volle prijs betalen en vanaf 55 jaar is de toegang weer gratis.
-# Maak een programma dat de te betalen prijs afdrukt nadat je de leeftijd hebt ingevoerd.
-#
-#
+# De basisprijs van een bioscoopkaartje is 12 euro.
+
+# - Kinderen tot 5 jaar zijn gratis
+# - kinderen van 5 tot 12 jaar betalen de halve prijs.
+# - Personen tussen 13 en 54 jaar moeten de volle prijs betalen
+# - vanaf 55 jaar is de toegang weer gratis.
+
+# Maak een programma dat de te betalen prijs afdrukt nadat je de leeftijd hebt ingevoerd als input.
+# Voorbeeld output: Voor de leeftijd 10 jaar is de prijs: 6.0
+# ==========================================
+
+leeftijd = int(input("voer je leeftijd in: "))
+
+
+
+# ==========================================
 # Opgave 3:
-# Schrijf een programma dat 3 gehele getallen sorteert. De getallen worden ingevoerd en respectievelijk opgeslagen in
-# de variabelen num1, num2 en num3. Het programma sorteert de getallen zodanig dat na afloop num1 ≤ num2 ≤ num3.
+# Schrijf een programma dat 3 gehele getallen (integers) sorteert. De willekeurige inputs worden opgeslagen in de variabelen num1, num2 en num3 (maak deze variabelen met inputs zelf aan). Schrijf een if statement die het laagste getal in num1 stopt, het middelste getal in num2 en het hoogste getal in num3.
+
+# Print de variabelen in de volgorde num1, num2, num3.
+# Voorbeeld input: 3 1 2
+# Voorbeeld output: 1 2 3
+# ==========================================
+
+
+
+# ==========================================
+# Opgave 4:
+# Gegeven is de variabele 'totaal' met een waarde van 0. Schrijf een programma dat herhaaldelijk een getal als input vraagt. Elk getal dat je invoert moet moet worden opgeteld bij het totaal. Als je 0 invoert moet het programma stoppen en met een print statement het totaal en het gemiddelde van de getallen afdrukken (dus totaal / aantal inputs). Als er geen getallen zijn ingevoerd moet de volgende string worden geprint: "er zijn geen getallen ingevoerd".
 #
-#
-# Opgave 4: Schrijf een programma dat gehele getallen leest, het totaal en het gemiddelde van deze getallen berekent
-# en afdrukt, waarbij 0 niet wordt meegeteld. Het programma eindigt met invoer 0.
-#
-#
-# Opgave 5: Schrijf een programma dat de tafel van “factor” afdrukt, nadat je een geheel getal tussen 0 en 10 (de
-# “factor”) hebt ingevoerd.
-#
-# Bijvoorbeeld als factor = 5:
-# Out:
-#
-# 1 x 5 = 5
-# 2 x 5 = 10
-# 3 x 5 = 15    # enz.
-#
-# Opgave 6:
-# Schrijf een programma met de volgende functionaliteit:
-#
-#     Eerst moet je een willekeurig getal invoeren (het “zoekgetal”).
-#     Daarna moet je een geheel getal tussen 0 en 10 invoeren (het “aantal”).
-#     Tenslotte voer je “aantal” getallen in (dus als aantal = 5 moet je 5 getallen invoeren).
-#
-# Het programma drukt alle getallen af die kleiner zijn dan het zoekgetal.
+# Voorbeeld input: 2, 4, 6, 0
+# Voorbeeld output: totaal: 12, gemiddelde: 4.0
+# ==========================================
+
+totaal = 0
+
+
+
+# ==========================================
+# Opgave 5:
+# Schrijf een input die een integer verwacht en stop deze in de variabele “factor”.
+# Schrijf daarna een programma dat de tafel van “factor” afdrukt, Print de tafel van 'factor' van 1 tot en met 10.
+
+# Voorbeeld input: 5
+# Voorbeeld output:
+#   1 x 5 = 5
+#   2 x 5 = 10
+#   3 x 5 = 15    # enz. tot en met 10
+# ==========================================
+
+
+
+
+
+
+
